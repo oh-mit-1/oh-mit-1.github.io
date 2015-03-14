a1b5d97107e60996a0eb3357cb0b8ba6e21bd1fc
Shauna <shaunagm@gmail.com>
2015-03-14 14:25:07 -0400

    Added pony

--- poem.js
+++ poem.js
@@ -1,6 +1,6 @@
 function nextLine(line) {
 
-	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on github</p>",
+	var poem = new Array("<p onClick=nextLine(1)>There once was a pony on github</p>",
 "<p onClick=nextLine(2)>So many pull requests she did snub</p>",
 "<p onClick=nextLine(3)>So I forked the repo</p>",
 "<p onClick=nextLine(4)>Now they all send <em>me</em> changes, d'oh!</p>",
